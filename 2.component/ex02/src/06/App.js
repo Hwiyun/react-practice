c312937ef72f2fec130fd8dc52c898c94f57196a
Hwiyun <sarakain123@gmail.com>
2023-03-09 13:44:19 +0900

    kanbanboard #1

--- 2.component/ex02/src/06/App.js
+++ 2.component/ex02/src/06/App.js
@@ -7,7 +7,7 @@ const StyledH1 = styled.h1`
     width: 180px;
     text-align: center;
     margin: 100px auto;
-    padding: 20px 20px 20px 20p;
+    padding: 20px 20px 20px 20px;
     border: 2px solid #999;
     color: #1144fe;
     background-color: #cdc1ce
@@ -15,9 +15,6 @@ const StyledH1 = styled.h1`
 
 const StyledDiv = styled.div`
     text-align: center;
-    color: #f00;
-    font-size: 20px;
-    font-weight: bold;
 `;
 
 // const Content = styled.p`
@@ -30,7 +27,7 @@ function App(props) {
     return (
         <StyledDiv id='App'>
             <StyledH1>CSS in JS</StyledH1>
-            <Content>
+            <Content color={'#f00'}>
                 Styled Components 연습
             </Content>
         </StyledDiv>
