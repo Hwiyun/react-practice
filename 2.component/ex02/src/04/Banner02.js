91b089bed49b6754955a09346714b9c7d6a4f963
Hwiyun <sarakain123@gmail.com>
2023-03-09 11:28:15 +0900

    styling #2

---
+++ 2.component/ex02/src/04/Banner02.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner02 from './Banner02';
+
+
+function Banner01(props) {
+    return (
+        <div>
+            <h1 className={styles.Header}>Hello React</h1>
+        </div>
+    );
+};
+
+
+export default Banner02;
