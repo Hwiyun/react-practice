3b45bfd86683b92ccec17e1b5fbcb986205fb758
Hwiyun <sarakain123@gmail.com>
2023-03-09 15:07:45 +0900

    kanbanboard #2

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -2,9 +2,10 @@ import React from 'react';
 import styles from './assets/scss/Card.scss';
 const Card = ({no, title, description}) => {
     return (
-            <div className = {styles.Card}>
-                <div className={styles.Card__title}>{title}</div>
-                <div className={styles.Card__Details}>
+        <div className = {styles.Card}>
+            <div className={styles.Card__title}>{title}
+            </div>
+            <div className={styles.Card__Details}>
                 {description}
             </div>
         </div>
