2d7af91be83a8308c2779ca1815aa123cf8025de
Hwiyun <sarakain123@gmail.com>
2023-03-13 16:39:49 +0900

    emaillist #1

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import './assets/scss/App.scss';
+import data from './assets/json/data.json'
 
+const App = () => {
+  const [emails, setEmails] = useState(data);
+  const notifyKeyWordChanged = function(keyword) {
+    // keyword가 firstName or lastName or email
+    const newEmails = emails.filter(function(e) {return})
 
-function App(props) {
-    return (
-        <div id='App'>
-            <h1>Emaillist</h1>
-        </div>
-    );
+  }
+
+  return (
+    <div id={'App'}>
+      <RegisterForm />
+      <SearchBar />
+      <Emaillist emails={emails} />
+    </div>
+  )
 }
 
 export default App;
