78ab7ebb72551067428e4486b16e9f784eac3a1a
Hwiyun <sarakain123@gmail.com>
2023-03-15 12:05:51 +0900

    kanbanboard

--- kanbanboard/frontend/src/Card.js
+++ kanbanboard/frontend/src/Card.js
@@ -3,13 +3,13 @@ import styles from './assets/scss/Card.scss';
 import TaskList from './TaskList';
 
 const Card = ({no, title, description}) => {
-    
     const [showDetail, setShowDetail] = useState(false);
     const [tasks, setTasks] = useState([]);
-
+    
     const addTask = async (taskName) => {
         console.log(taskName);
     }
+
     return (
         <div className={styles.Card}>
             <div
@@ -20,10 +20,30 @@ const Card = ({no, title, description}) => {
                 }
                 onClick={async () => {
                     if(!showDetail) {
-                        const response = await fetch
+                        try {
+                            const response = await fetch(`/api/task?cardNo=${no}`, {
+                                method: 'get',
+                                headers: {
+                                    'Accept': 'application/json'
+                                }
+                            });
+                
+                            if(!response.ok) {
+                                throw new Error(`${response.status} ${response.statusText}`);
+                            }
+                
+                            const json = await response.json();
+                            if(json.result !== 'success') {
+                                throw new Error(`${json.result} ${json.message}`)
+                            }
+                
+                            setTasks(json.data);
+                        } catch(err) {
+                            console.log(err.message);
+                        }                        
                     }
-                 
-                setShowDetail(!showDetail)
+
+                    setShowDetail(!showDetail);
                 }}>
                 {title}
             </div>            
@@ -31,7 +51,7 @@ const Card = ({no, title, description}) => {
                 showDetail ?    
                     <div className={styles.Card__Details}>
                         {description}
-                        <TaskList 
+                        <TaskList
                             cardNo={no}
                             tasks={tasks}
                             callbackAddTask={addTask}/>
