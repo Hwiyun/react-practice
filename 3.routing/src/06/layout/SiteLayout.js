43fd531799c94db6664b9c0d8db14991e8fe8fa0
Hwiyun <sarakain123@gmail.com>
2023-03-16 16:55:20 +0900

    routing #2

--- 3.routing/src/06/layout/SiteLayout.js
+++ 3.routing/src/06/layout/SiteLayout.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Header from './Header';
 import Navigation from './Navigation';
 import Footer from './Footer';
 import styles from '../assets/scss/layout/Content.scss';
-
 const SiteLayout = ({children}) => {
     return (
         <Fragment>
             <Header/>
-            <div className={styles.Main}>
+            <div className={styles.Content}>
                 {children}
             </div>
             <Navigation/>
